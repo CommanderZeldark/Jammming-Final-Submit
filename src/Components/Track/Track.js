a9b517d066144e9927abd333a3731bc5f8bc4dca
Commander Zeldark <35015061+CommanderZeldark@users.noreply.github.com>
2018-03-03 13:07:23 -0800

    Initial commit

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Track.css';
+
+const song3 = {
+  id: 3,
+  name: "Engage",
+  artist: "Captain Picard and the Next Generation",
+  album: "Warp 9.9"
+}
+
+class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack() {
+    this.props.onAdd(this.props.track);
+  }
+
+  removeTrack() {
+    this.props.onRemove(this.props.track);
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.props.track.album}</p>
+        </div>
+        <a className="Track-action">{!this.props.isRemoval ? <div onClick={this.addTrack}>+</div> : <div onClick={this.removeTrack}>-</div>}</a>
+      </div>
+    );
+  }
+}
+
+export default Track;
